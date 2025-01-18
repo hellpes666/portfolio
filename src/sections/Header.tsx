8e7be6c03d203cb47114cb8a9603785d5d456146
Barmatov Alexey <alexrui_hpow@mail.ru>
2025-01-18 22:51:46 +0300

    add header component

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,50 @@
+type THeaderTitle = 'Home' | 'Projects' | 'About' | 'Contact';
+
+interface IHederItems {
+  link: string;
+  title: THeaderTitle;
+  active: boolean;
+}
 export const Header = () => {
-  return <div>Header Section</div>;
+  const HEADER_ITEMS: IHederItems[] = [
+    {
+      link: '#',
+      title: 'Home',
+      active: false,
+    },
+    {
+      link: '#',
+      title: 'Projects',
+      active: false,
+    },
+    {
+      link: '#',
+      title: 'About',
+      active: false,
+    },
+    {
+      link: '#',
+      title: 'Contact',
+      active: true,
+    },
+  ];
+
+  const activeNavItemStyle: string =
+    'bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900';
+
+  return (
+    <header className="flex justify-center items-center relative top-3">
+      <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+        {HEADER_ITEMS.map((item, index) => (
+          <a
+            href={item.link}
+            className={`nav-item ${item.active ? activeNavItemStyle : ''}`}
+            key={index}
+          >
+            {item.title}
+          </a>
+        ))}
+      </nav>
+    </header>
+  );
 };
