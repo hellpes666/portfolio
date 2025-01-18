5718a5db93590e10de3fa42396505242bd601d03
Barmatov Alexey <alexrui_hpow@mail.ru>
2025-01-18 23:26:48 +0300

    add hero section
    it should be continued and refactoring for a view of clean code by Bobby :)

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -33,7 +33,7 @@ export const Header = () => {
     'bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900';
 
   return (
-    <header className="flex justify-center items-center relative top-3">
+    <header className="flex justify-center items-center  top-3 fixed w-full">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
         {HEADER_ITEMS.map((item, index) => (
           <a
